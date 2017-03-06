0cc6e1b9a1d52e44e3d47852182288c719bec4d8
Ashley <ashley@containership.io>
2017-03-06 13:22:57 -0500

    split initalize into runLeader and runFollower

--- application.js
+++ application.js
@@ -2,25 +2,26 @@
 
 const logs = require('./lib/logs');
 
-const _ = require('lodash');
 const ContainershipPlugin = require('containership.plugin');
 
+const APPLICATION_NAME = 'containership-logs';
 
 module.exports = new ContainershipPlugin({
     type: 'core',
 
-    initialize: function(core) {
-        const DEFAULT_LOG_PATH = core.options['base-log-dir'] || '/var/log/containership';
-        const application_name = 'containership-logs';
-        core.logger.register(application_name);
+    runFollower: (core) => {
+        core.loggers[APPLICATION_NAME].log('verbose', `${APPLICATION_NAME} does not run on follower nodes.`);
+    },
 
+    runLeader: (core) => {
         const add_application = () => {
+            const DEFAULT_LOG_PATH = core.options['base-log-dir'] || '/var/log/containership';
             core.cluster.myriad.persistence.get(
-                    [core.constants.myriad.APPLICATION_PREFIX, application_name].join(core.constants.myriad.DELIMITER),
+                    [core.constants.myriad.APPLICATION_PREFIX, APPLICATION_NAME].join(core.constants.myriad.DELIMITER),
                     (err) => {
                         if(err) {
                             return core.applications.add({
-                                id: application_name,
+                                id: APPLICATION_NAME,
                                 image: 'containership/docker-cs-logs:latest',
                                 cpus: 0.1,
                                 memory: 64,
@@ -30,7 +31,7 @@ module.exports = new ContainershipPlugin({
                                         per_host: 1
                                     },
                                     metadata: {
-                                        plugin: application_name,
+                                        plugin: APPLICATION_NAME,
                                         ancestry: 'containership.plugin'
                                     }
                                 },
@@ -44,32 +45,34 @@ module.exports = new ContainershipPlugin({
                                     }
                                 ]
                             }, () => {
-                                core.loggers[application_name].log('verbose', ['Created ', application_name, '!'].join(''));
+                                core.loggers[APPLICATION_NAME].log('verbose', `Created ${APPLICATION_NAME}!`);
                             });
                         }
 
-                        return core.loggers[application_name].log('verbose', [application_name, 'already exists, skipping create!'].join(' '));
+                        return core.loggers[APPLICATION_NAME].log('verbose', `${APPLICATION_NAME} already exists, skipping create!`);
                     }
             );
         };
 
-        if('leader' === core.options.mode) {
-            if(core.cluster.praetor.is_controlling_leader()) {
-                add_application();
-            }
-
-            core.cluster.legiond.on('promoted', () => {
-                core.cluster.myriad.persistence.keys(core.constants.myriad.APPLICATIONS, (err, applications) => {
-                    if(err || !_.isEmpty(applications)) {
-                        return add_application();
-                    }
-
-                    return setTimeout(add_application, 2000);
-                });
-            });
-
-            return logs.Init(core).register_routes();
+        if(core.cluster.praetor.is_controlling_leader()) {
+            add_application();
         }
+
+        core.cluster.legiond.on('myriad.bootstrapped', () => {
+            add_application();
+        });
+
+        return logs.Init(core).register_routes();
+    },
+
+    initialize: function(core) {
+        core.logger.register(APPLICATION_NAME);
+
+        if(core.options.mode === 'leader') {
+            return module.exports.runLeader(core);
+        }
+
+        return module.exports.runFollower(core);
     },
 
     reload: function() {}
